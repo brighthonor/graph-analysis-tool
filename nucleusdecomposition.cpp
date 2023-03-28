a5d498e69985f0870d764470b3a624fb83e194a0
Sanghyun Yi <shyi0211@gmail.com>
2023-03-29 00:32:51 +0900

    commit for remote dev

--- nucleusdecomposition.cpp
+++ nucleusdecomposition.cpp
@@ -47,6 +47,101 @@ bool NucleusDecomposition::writeToFileStat(std::string graph_name, bool directed
     return true;
 }
 
+////////////// BHeap Construct //////////////
+struct NucleusDecomposition::BHeap *NucleusDecomposition::construct(unsigned n_max) {
+    unsigned i;
+    auto *heap = new BHeap();
+    heap->n_max = n_max;
+    heap->n = 0;
+    heap->pt.resize(n_max);
+    for(i=0; i<n_max; i++) heap->pt[i] = -1;
+    heap->kv.resize(n_max);
+    return heap;
+}
+
+void NucleusDecomposition::swap(struct BHeap *heap, unsigned i, unsigned j) {
+    KeyValue kv_tmp = heap->kv[i];
+    unsigned pt_tmp = heap->pt[kv_tmp.key];
+    heap->pt[heap->kv[i].key] = heap->pt[heap->kv[j].key];
+    heap->kv[i] = heap->kv[j];
+    heap->pt[heap->kv[j].key] = pt_tmp;
+    heap->kv[j] = kv_tmp;
+}
+
+void NucleusDecomposition::bubbleUp(struct BHeap *heap, unsigned i) {
+    unsigned j = (i-1)/2;
+    while(i>0) {
+        if(heap->kv[j].value > heap->kv[i].value) {
+            swap(heap, i, j);
+            i = j;
+            j = (i-1)/2;
+        } else break;
+    }
+}
+
+void NucleusDecomposition::bubbleDown(struct BHeap *heap) {
+    unsigned i=0, j1=1, j2=2, j;
+    while(j1 < heap->n) {
+        j = ((j2 < heap->n) && (heap->kv[j2].value < heap->kv[j1].value)) ? j2 : j1;
+        if(heap->kv[j].value < heap->kv[i].value) {
+            swap(heap, i, j);
+            i = j;
+            j1 = 2*i+1;
+            j2 = j1+1;
+            continue;
+        }
+        break;
+    }
+}
+
+void NucleusDecomposition::insert(struct BHeap *heap, struct KeyValue kv) {
+    heap->pt[kv.key] = (heap->n)++;
+    heap->kv[heap->n-1] = kv;
+    bubbleUp(heap, heap->n-1);
+}
+
+void NucleusDecomposition::update(struct BHeap *heap, unsigned key) {
+    unsigned i = heap->pt[key];
+    if(i!=-1) {
+        ((heap->kv[i]).value)--;
+        bubbleUp(heap, i);
+    }
+}
+
+struct NucleusDecomposition::KeyValue NucleusDecomposition::popmin(struct BHeap *heap) {
+    KeyValue min = heap->kv[0];
+    heap->pt[min.key] = -1;
+    heap->kv[0] = heap->kv[--(heap->n)];
+    heap->pt[heap->kv[0].key] = 0;
+    bubbleDown(heap);
+    return min;
+}
+
+/////////// Building BHeap ///////////////
+struct NucleusDecomposition::BHeap* NucleusDecomposition::makeHeap() {
+    unsigned i;
+    struct KeyValue kv;
+    struct BHeap *heap = construct(scsHasr.size());
+
+    K.resize(scsHasr.size(), -1);
+
+    for(i=0; i<scsHasr.size(); i++) {
+        if(scsHasr[i].empty()){
+            K[i] = 0;
+            continue;
+        }
+        kv.key = i;
+        kv.value = scsHasr[i].size();
+        insert(heap, kv);
+    }
+    return heap;
+}
+
+void NucleusDecomposition::freeHeap(struct BHeap *heap) {
+    free(heap);
+}
+
+//////////// Nucleus Decomposition ////////////
 void NucleusDecomposition::findRSCliques(USGraph &graph, int r, int s) { // s should be always larger than r
     if(s < r) {
         int tmp = s;
@@ -82,7 +177,44 @@ void NucleusDecomposition::findRSCliques(USGraph &graph, int r, int s) { // s sh
     }
 }
 
-void NucleusDecomposition::nd() {
+void NucleusDecomposition::nd(USGraph &graph, int *max34) {
+    int fc_t = 0;
 
+    cid = 0;
+    nSubcores = 0;
+    component.resize(rcliques.size(), -1);
+
+    struct BHeap *heap = makeHeap();
+
+    for(int i=0; i<rcliques.size(); i++) {
+        int t;
+        int val;
+        struct KeyValue kv = popmin(heap);
+        t = kv.key;
+        val = kv.value;
+
+        unassigned.clear();
+        subcore sc (val);
+        skeleton.push_back(sc);
+
+        fc_t = K[t] = val;
+
+        for(auto sc: scsHasr[t]) {
+            bool nv = false;
+            for(auto rc: rcsIns[sc]) {
+                if(K[rc]!=-1){
+                    nv = true;
+                    break;
+                }
+            }
+
+            if(nv) {
+                for(auto rc: rcsIns[sc]) {
+                    if(heap->pt)
+                }
+            }
+        }
+    }
 }
+
 } // nmaespace snu
--- nucleusdecomposition.h
+++ nucleusdecomposition.h
@@ -20,6 +20,42 @@ class NucleusDecomposition : public UndirectedStat {
     void nd();
     void findRSCliques(USGraph &graph, int r, int s);
 
+    // nd tree
+    struct nd_tree_node {
+        int id_;
+        int parent_;
+        std::vector<int> children_;
+        std::vector<int> vertices_;
+        uint32_t k_;
+        uint32_t r_;
+        uint32_t s_;
+        uint32_t num_edges_;
+        double density_;
+    };
+
+    // for nBucket (MinHeap)
+    struct KeyValue {
+        unsigned key;
+        unsigned value;
+    };
+
+    struct BHeap {
+        unsigned n_max;
+        unsigned n;
+        std::vector<unsigned> pt;
+        std::vector<KeyValue> kv;
+    };
+
+    struct BHeap *construct(unsigned n_max);
+    void swap(struct BHeap *heap, unsigned i, unsigned j);
+    void bubbleUp(struct BHeap *heap, unsigned i);
+    void bubbleDown(struct BHeap *heap);
+    void insert(struct BHeap *heap, struct KeyValue kv);
+    void update(struct BHeap *heap, unsigned key);
+    struct KeyValue popmin(struct BHeap *heap);
+    struct BHeap *makeHeap();
+    void freeHeap(struct BHeap *heap);
+
     struct subcore {
         bool visible;
         int rank;
@@ -30,6 +66,17 @@ class NucleusDecomposition : public UndirectedStat {
         int size;
         int nEdge;
         double ed;
+
+        subcore(int k) {
+            K = k;
+            rank = 0;
+            parent = -1;
+            root = -1;
+            visible = true;
+            size = 0;
+            nEdge = 0;
+            ed = -1;
+        }
     };
 
     std::vector<std::set<Graph::Vid>> rcliques;
@@ -43,6 +90,9 @@ class NucleusDecomposition : public UndirectedStat {
     std::vector<std::pair<int, int>> relations;
     std::vector<int> unassigned;
     int nSubcores;
+
+    std::vector<int> K;
+    std::vector<nd_tree_node> nd_tree;
 };
 }
 
